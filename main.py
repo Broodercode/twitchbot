6c9101bd0ffc6c7c464224581daab25b9d51c9ef
broodercode <c_schiebelbein@hotmail.com>
2024-03-25 05:27:51 -0600

    tweaking greeting

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class Bot(commands.Bot):
 
     def __init__(self):
         super().__init__(token=ACCESS_TOKEN, prefix='', initial_channels=[
-            'broodvx',
+            'broodvx'
         ])
 
     async def event_ready(self):
@@ -23,25 +23,37 @@ class Bot(commands.Bot):
         print(f'User id is | {self.user_id}')
 
     async def event_message(self, ctx):
-        self.log_message(ctx)
+        # self.log_message(ctx)
         self.handle_wow(ctx)
         self.handle_bro(ctx)
         self.handle_broodv1Brud(ctx)
         await self.handle_greeting(ctx)
-        self.log_lol(ctx)
 
-    def log_message(self, ctx):
-        dt = datetime.now(timezone.utc)
-        ts = dt.strftime("%Y/%m/%d %H:%M:%S.%f")
+## -- The following code is for debugging purposes -- ##
+        # if ctx.author:
+        #     print(f"Message from {ctx.author.name}: {ctx.content}")
+        # else:
+        #     print(f"Received a message without an author: {ctx.content}")
 
-        user_id = str(ctx.author.id)
-        user_handle = str(ctx.author.name)
-        content = str(ctx.content)
-        msg_id = str(ctx.tags['id'])
-        room_id = str(ctx.tags['room-id'])
-        created_at = str(ts)
-        data = {"user_id": user_id, "content": content, "msg_id": msg_id,
-                "room_id": room_id, "created_at": created_at, "user_handle": user_handle}
+        # if ctx.author and ctx.author.name.lower() == self.nick.lower():
+        #     return
+## -- The above code is for debugging purposes -- ##
+
+
+        # self.log_lol(ctx)
+
+    # def log_message(self, ctx):
+    #     dt = datetime.now(timezone.utc)
+    #     ts = dt.strftime("%Y/%m/%d %H:%M:%S.%f")
+
+    #     user_id = str(ctx.author.id)
+    #     user_handle = str(ctx.author.name)
+    #     content = str(ctx.content)
+    #     msg_id = str(ctx.tags['id'])
+    #     room_id = str(ctx.tags['room-id'])
+    #     created_at = str(ts)
+    #     data = {"user_id": user_id, "content": content, "msg_id": msg_id,
+    #             "room_id": room_id, "created_at": created_at, "user_handle": user_handle}
 
         # try:
         #     # Replace with your own url
@@ -71,7 +83,7 @@ class Bot(commands.Bot):
 
     def handle_bro(self, ctx):
         if ('bro' in ctx.content and not 'brood' in ctx.content and ctx.tags['room-id'] == '622249091') or \
-           ('Bro' in ctx.content and not 'Brood' in ctx.tags['room-id'] == '622249091') or \
+           ('Bro' in ctx.content and not 'Brood' in ctx.content and ctx.tags['room-id'] == '622249091') or \
            ('brah' in ctx.content and ctx.tags['room-id'] == '622249091') or \
            ('bruh' in ctx.content and ctx.tags['room-id'] == '622249091') or \
            ('breh' in ctx.content and ctx.tags['room-id'] == '622249091') or \
@@ -105,34 +117,39 @@ class Bot(commands.Bot):
 
     async def handle_greeting(self, ctx):
         filename = 'greetingList.txt'
-        user = ctx.author.name
-
-        # Open the file in read mode to check if the user is in the list
-        with open(filename, "r") as f:
-            users = f.readlines()  
-            users = [x.strip() for x in users]  # Remove the newline character from each user
-
-        # If the user is not in the list, open the file in append mode to add them
-        if user not in users:
-            with open(filename, "a") as f_append:
-                f_append.write(user + "\n")
-                # Send a greeting message to the channel
-                await ctx.channel.send(f"Broodvx welcomes you to today's stream stream, {user}!")  # Use 'ctx.channel.send' to send the message
+        # Check if ctx.author is not None before accessing its attributes
+        if ctx.author is not None:
+            user = ctx.author.name
+            # Further code to handle greeting...
 
 
+            with open(filename, "r") as f:
+                users = f.readlines()  
+                users = [x.strip() for x in users]  # Remove the newline character from each user
+            # If the user is not in the list, open the file in append mode to add them
+            if user not in users:
+                with open(filename, "a") as f_append:
+                    f_append.write(user + "\n")
+                    # Send a greeting message to the channel
+                    await ctx.channel.send(f"Hey! BroodVX welcomes you to the stream, {user}!")  # Use 'ctx.channel.send' to send the message
+                    playsound('./audio/hey.mp3')
+        else:
+            return
+            # Open the file in read mode to check if the user is in the list
 
-    def log_lol(self, ctx):
-        lol_keywords = ['lol', 'LOL', 'LUL', 'KEKW', 'haha', 'lmao', 'LMAO', 'LMFAO', 'lmfao']
-        room_ids = ['622249091', '171270662', '30009229']
-        filenames = ['brood.txt', 'throgg.txt', 'zheal.txt']
 
-        for keyword, room_id, filename in zip(lol_keywords, room_ids, filenames):
-            if keyword in ctx.content and ctx.tags['room-id'] == room_id:
-                dt = datetime.now(timezone.utc)
-                ts = dt.strftime("%Y/%m/%d %H:%M:%S.%f")
+    # def log_lol(self, ctx):
+    #     lol_keywords = ['lol', 'LOL', 'LUL', 'KEKW', 'haha', 'lmao', 'LMAO', 'LMFAO', 'lmfao']
+    #     room_ids = ['622249091', '171270662', '30009229']
+    #     filenames = ['brood.txt', 'throgg.txt', 'zheal.txt']
 
-                with open(filename, "a") as f:
-                    f.write(f"{ts}: {ctx.author.name}: {ctx.content}\n")
+    #     for keyword, room_id, filename in zip(lol_keywords, room_ids, filenames):
+    #         if keyword in ctx.content and ctx.tags['room-id'] == room_id:
+    #             dt = datetime.now(timezone.utc)
+    #             ts = dt.strftime("%Y/%m/%d %H:%M:%S.%f")
+
+    #             with open(filename, "a") as f:
+    #                 f.write(f"{ts}: {ctx.author.name}: {ctx.content}\n")
 
 scheduler = BackgroundScheduler() 
 scheduler.add_job(stream_ping, 'interval', seconds=180) 
